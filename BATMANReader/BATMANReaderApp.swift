c3f91737ae3d98dd51877c33998ca5e59104a7c1
ervinleeumich-hub <ervinlee.umich@gmail.com>
2026-06-10 20:33:27 -0500

    Initial Commit

---
+++ BATMANReader/BATMANReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  BATMANReaderApp.swift
+//  BATMANReader
+//
+//  Created by Ervin Lee on 6/10/26.
+//
+
+import SwiftUI
+
+@main
+struct BATMANReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
